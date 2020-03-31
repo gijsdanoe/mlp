21ae48f648ee14fe534db02e48019284879ea5f7
lennartschepers <lenschepers@github.com>
2020-03-31 15:56:46 +0200

    fixed data to tuple method

--- test.py
+++ test.py
@@ -3,10 +3,13 @@ import pandas as pd
 
 def read_files():
     print("\n##### Reading files...")
-    csvfile = open('data/data.csv', 'r', encoding='UTF-8').readlines()
-    csvdict = tuple()
+    csvfile = open('OnionOrNot.csv', 'r', encoding='UTF-8').readlines()
+    csvlist = list()
     for line in csvfile:
-        print(line[-2])
+        line = line.strip('"')
+        itemtuple = (line[-2], str(line[:-3]))
+        csvlist.append(itemtuple)
+    print(csvlist)
     #df = pd.read_csv('data/data.csv', index_col = 0, skiprows=1).dict()
     #for sentence,category in df:
         #print(f"{sentence} {row}")
