38aa3fbed1af68217bfe9716a24d505512e34cf0
Gijs <g.danoe@student.rug.nl>
2020-04-13 18:32:31 +0200

    final

--- assignment_classification_stan.py
+++ assignment_classification_stan.py
@@ -2,47 +2,47 @@
 
 import nltk.classify
 from nltk.tokenize import word_tokenize
-from featx import bag_of_words, high_information_words, bag_of_non_stopwords, bag_of_words_in_set, bag_of_bigrams_words, bag_of_words_not_in_set
-from classification import precision_recall
+from featx import bag_of_words, high_information_words, bag_of_non_stopwords, bag_of_words_in_set, bag_of_bigrams_words, \
+    bag_of_words_not_in_set
 from nltk.classify import SklearnClassifier
 from sklearn.svm import LinearSVC, SVC
+from sklearn.model_selection import GridSearchCV
 from nltk.stem.snowball import SnowballStemmer
+from sklearn.feature_extraction import DictVectorizer
 import string
-
-from random import shuffle
-from os import listdir  # to read files
-from os.path import isfile, join  # to read files
-import sys
-
-import pandas as pd
 
 
 def read_files():
     csvfile = open('OnionOrNot.csv', 'r', encoding='UTF-8').readlines()
     feats = list()
-    bigrams = list()
     for line in csvfile:
-        line = line.strip('"')
         data = str(line[:-3]).lower()
         category = line[-2]
-        tokens = word_tokenize(data)
-        punct = set(string.punctuation)
-        for item in tokens:
-            if item in punct:
-                tokens.remove(item)
-            else:
-                pass
-        no_stop = bag_of_non_stopwords(tokens)
-        stemmer = SnowballStemmer("english")
-        filteredtokens = [stemmer.stem(filteredtoken) for filteredtoken in no_stop]
-        feats.append((bag_of_words(filteredtokens), category))
+        if category == '0' or category == '1':
+            tokens = word_tokenize(data)
 
+            # stemming
+            # stemmer = SnowballStemmer("english")
 
+            # punctuation removal
+            # tokens = [stemmer.stem(filteredtoken) for filteredtoken in tokens]
+            # punct = set(string.punctuation)
+            # for item in tokens:
+            #     if item in punct:
+            #         tokens.remove(item)
+            #     else:
+            #         pass
 
+            # stopwords removal
+            # tokens = bag_of_non_stopwords(tokens)
+
+            feats.append((bag_of_words(tokens), category))
+        else:
+            pass
     return feats
 
 
-# splits a labelled dataset into two disjoint subsets train and test
+# splits a labelled dataset into train, dev and test sets
 def split_data(feats):
     split_1 = int(0.8 * len(feats))
     split_2 = int(0.9 * len(feats))
@@ -54,17 +54,14 @@ def split_data(feats):
 
 # trains a classifier
 def train(train_feats):
-    classifier = nltk.classify.SklearnClassifier(LinearSVC())  # linear
-    # classifier = nltk.classify.SklearnClassifier(SVC(kernel='poly', C=100)) #poly
-    # classifier = nltk.classify.SklearnClassifier(SVC(kernel='rbf', C=100)) #rbf
-    # classifier = nltk.classify.NaiveBayesClassifier.train(train_feats)
+    classifier = nltk.classify.SklearnClassifier(SVC(kernel='rbf', C=100, gamma=0.001))  # SVM
     classifier.train(train_feats)
+    # nltk.classify.NaiveBayesClassifier.train(train_feats)  # Naive Bayes
     return classifier
 
 
 # obtain the high information words
 def high_information(feats, categories):
-
     from collections import defaultdict
     words = defaultdict(list)
     all_words = list()
@@ -78,12 +75,8 @@ def high_information(feats, categories):
             words[category].append(w)
             all_words.append(w)
 
-
     labelled_words = [(category, words[category]) for category in categories]
-
     high_info_words = set(high_information_words(labelled_words, min_score=5))
-
-
     return high_info_words
 
 
@@ -95,6 +88,7 @@ def high_info_feats(feats, high_info_words):
         high_info_feats.append((highinfo, feat[1]))
     return high_info_feats
 
+
 def bigram(feats):
     bigrams = []
     for feat in feats:
@@ -104,31 +98,41 @@ def bigram(feats):
         bigrams.append(bag_of_words(cleanbigram))
     return bigrams
 
+def gridsearch(X_train, y_train):
+    param_grid = {'C': [0.1, 1, 10, 100], 'gamma': [1, 0.1, 0.01, 0.001], 'kernel': ['linear','rbf', 'poly', 'sigmoid']}
+    grid = GridSearchCV(SVC(), param_grid, refit=True, verbose=2)
+    grid.fit(X_train, y_train)
+    return grid.best_estimator_
 
 def main():
-    categories = ['0', '1']
     feats = read_files()
 
+    # bigrams
+    # bigrams = bigram(feats)
+    # for i, j in zip(feats, bigrams):
+    #     i[0].update(j)
+
     # high information words
+    categories = ['0', '1']
     highinfo = high_information(feats, categories)
     highinfo_feats = high_info_feats(feats, highinfo)
+    feats = highinfo_feats
 
-    # bigrams. works with both feats and highinfo_feats
-    bigrams = bigram(feats)
-    for i,j in zip(highinfo_feats, bigrams):
-        i[0].update(j)
 
-    train_feats, dev_feats, test_feats = split_data(highinfo_feats)
+    train_feats, dev_feats, test_feats = split_data(feats)
 
     classifier = train(train_feats)
     score = nltk.classify.accuracy(classifier, test_feats)
-
     print(score)
 
+    # grid search
+    # vectorizer = DictVectorizer()
+    # X_train, y_train = list(zip(*train_feats))
+    # X_train = vectorizer.fit_transform(X_train)
+    # best_param = gridsearch(X_train, y_train)
+    # print(best_param)
 
 
 
 if __name__ == '__main__':
     main()
-
-
--- test.py
+++ test.py
@@ -1,17 +1,42 @@
 #!/usr/bin/env python3
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from featx import bag_of_words
+from nltk.tokenize import word_tokenize
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import LinearSVC, SVC
 
 def read_files():
-    print("\n##### Reading files...")
     csvfile = open('OnionOrNot.csv', 'r', encoding='UTF-8').readlines()
-    csvlist = list()
+    x = list()
+    y = list()
     for line in csvfile:
-        line = line.strip('"')
-        itemtuple = (str(line[:-3]), line[-2])
-        csvlist.append(itemtuple)
+        data = str(line[:-3]).lower()
+        category = line[-2]
+        tokens = word_tokenize(data)
+        x.append([tokens])
+        y.append([category])
+    z = [i == 'a' for i in y]
+    return x,y,z
 
-    #df = pd.read_csv('data/data.csv', index_col = 0, skiprows=1).dict()
-    #for sentence,category in df:
-        #print(f"{sentence} {row}")
+def split(x,y):
+    X_train, X_test, y_train, y_test = train_test_split(x,y, test_size=0.3, random_state=109)
+    return X_train, y_train
 
-read_files()
+def gridsearch(train_feats, dev_feats):
+    param_grid = {'C': [0.1, 1, 10, 100], 'gamma': [1, 0.1, 0.01, 0.001], 'kernel': ['linear','rbf', 'poly', 'sigmoid']}
+    grid = GridSearchCV(SVC(), param_grid, refit=True, verbose=2)
+    grid.fit(train_feats, dev_feats)
+    return grid.best_estimator_
+
+def main():
+    x,y,z = read_files()
+    #X_train, y_train = split(x,y)
+    print(x)
+    print(y)
+    print(z)
+
+    best_param = gridsearch(x, z)
+    print(best_param)
+
+main()
